10aa994e750bce9800c023561c1f5bf21819b581
josepaniagua <jpanjr@gmail.com>
2019-09-09 23:15:35 -0700

    jsx is acting wierd

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      React
-    </div>
+      <div>
+        <p>Hello</p>
+        <p>World</p>
+      </div>
   );
 }
 
